8150f6840b6cc79d3e43535829d5aaef0b72f7bf
Tripoli80 <Tripoli80@gmail.com>
2022-06-08 19:57:55 +0200

    All 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refBoxes = document.querySelector("#boxes");
+const refCountBoxes = document.querySelector("input");
+console.log("refCountBoxes :>> ", refCountBoxes);
+const refBtnCreate =document.querySelector("button[data-create]")
+const refBtnDestroy = document.querySelector("button[data-destroy]");
+
+refBtnCreate.addEventListener("click", () => {
+const refCountBoxes = document.querySelector("input");
+  createBoxes(Number(refCountBoxes.value))
+});
+
+refBtnDestroy.addEventListener("click", () => {
+  createBoxes(0);
+});
+
+
+
+function createBoxes(amount) {
+  console.log(amount);
+  refBoxes.innerHTML=""
+  if (amount>0){const boxes = []
+  for (let i = 0; i < amount; i += 1)
+  {
+    const tempElement = document.createElement("div")
+    tempElement.style.width = `${30 + i * 10}px`
+    tempElement.style.height = `${30 + i * 10}px`;
+    tempElement.style.backgroundColor = getRandomHexColor();
+    tempElement.textContent = i + 1;
+    boxes.push(tempElement)  
+  }
+refBoxes.append(...boxes)}
+  
+}
