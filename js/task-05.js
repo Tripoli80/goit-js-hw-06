7989eac60231cfb40e2acc92b79418d8a3dc3937
Tripoli80 <Tripoli80@gmail.com>
2022-06-09 13:27:26 +0200

    event.key === "Escape"

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,18 @@
 const refInput = document.querySelector("#name-input");
 const refNameOutput = document.querySelector("#name-output");
 
+window.addEventListener("keydown", clearTextArea);
 refInput.addEventListener("input", changeTextArea);
 
 function changeTextArea(event) {
   refNameOutput.textContent = event.currentTarget.value
     ? event.currentTarget.value
     : "Anonymous";
+}
+
+function clearTextArea(event) {
+  if (event.key === "Escape") {
+    refInput.value = "";
+    changeTextArea(event);
+  }
 }
